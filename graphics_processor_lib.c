70dd2609cc9ad8e8ba5bc6b53dbdf1d7253ff67a
Leticia Ribeiro <68934064+leticiaribeiro7@users.noreply.github.com>
2024-05-31 00:05:22 -0300

    Update graphics_processor_lib.c

--- graphics_processor_lib.c
+++ graphics_processor_lib.c
@@ -5,6 +5,7 @@
 #include <asm/io.h>
 #include "./address_map_arm.h"
 
+
 // Module metadata
 MODULE_AUTHOR("TEC499-TP02-G02");
 MODULE_DESCRIPTION("Módulo pro processador gráfico");
@@ -14,6 +15,14 @@ void * LW_virtual; // Lightweight bridge base address
 volatile int* data_a_ptr;
 volatile int* data_b_ptr;
 volatile int* start_ptr;
+
+static struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .open = device_open,
+    .read = device_read,
+    .write = device_write,
+    .release = device_release,
+};
 
 static void __init iniciar (void) {
     // generate a virtual address for the FPGA lightweight bridge
@@ -27,6 +36,20 @@ static void __init iniciar (void) {
 
 static void __exit parar(void) {
     iounmap (LW_virtual);
+}
+
+// funçoes pro driver de dispositivo de caracteres
+static int device_open(struct inode *inode, struct file *file) {
+    return SUCCESS;
+}
+static int device_release(struct inode *inode, struct file *file) {
+    return 0;
+}
+
+static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t *offset) {
+}
+
+static ssize_t device_write(struct file *filp, const char *buffer, size_t length, loff_t *offset) {
 }
 
 static int set_background_color(int R, int G, int B) {
