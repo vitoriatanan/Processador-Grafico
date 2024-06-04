efef667dc7cb96a59331468550f18d56cf311dfa
vitoriatanan <128485508+vitoriatanan@users.noreply.github.com>
2024-06-04 10:18:22 -0300

    Update monitorTest2.c

--- monitorTest2.c
+++ monitorTest2.c
@@ -3,11 +3,12 @@
 #include <sys/mman.h>
 #include "./address_map_arm.h"
 
+/*
 #define LW_BRIDGE_SPAN 0x00005000
 #define LW_BRIDGE_BASE 0xFF200000
 #define data_a_base 0x80 //DATA_A
 #define data_b_base 0x70 //DATA_B
-#define WERREG 0xc0 //start
+#define WERREG 0xc0 //start*/
 
 //
 void escrita_buffer(volatile int* wrreg_ptr) {
@@ -16,25 +17,39 @@ void escrita_buffer(volatile int* wrreg_ptr) {
 }
 
 //define cor do background
-int set_background_color(int R, int G, int B, volatile int* data_a,  volatile int* data_b, volatile int* wrreg) {
-    *data_a = (0x0000 | 0x00000); //opcode para WBR e endereço do registrador
-    *data_b = (R << 2) | (G << 4) | B; 
+int set_background_color (int R, int G, int B, volatile int* data_a,  volatile int* data_b, volatile int* wrreg) {
+    *data_a = 0x0000; //opcode para WBR e endereço do registrador
+    *data_b = (B << 6) | (G << 4) | R;
     
     escrita_buffer(wrreg);
+    return 1;
 }
 
-//offset: 0b000010000
+//seta um sprite
 int set_sprite (int registrador, int x, int y, int offset, int activation_bit, volatile int* data_a, volatile int* data_b, volatile int* wrreg) {
     *data_a = (registrador << 4) | 0x0000;
     *data_b = (activation_bit << 29) | (x << 19) | (y << 9) | offset;
     
     escrita_buffer(wrreg);
+    return 1;
+}
+
+//edita um bloco no background 80x60
+int set_background_block (int column, int line, int R, int G, int B, volatile int* data_a, volatile int* data_b, volatile int* wrreg) {
+    //Calcula o endereço de memória do bloco com base na coluna e linha
+    int endereco_memoria = (line * 80 + column); // 80: numero de colunas por linha = 245
+
+    *data_a = (endereco_memoria << 4) | 0b0010;
+    *data_b = (B << 6) | (G << 4) | R;
+
+    escrita_buffer(wrreg);
+    return 1;
 }
 
 int main() {
     volatile int* data_a_ptr;
     volatile int* data_b_ptr;
-    volatile int* start_ptr;
+    //volatile int* start_ptr;
     volatile int* wrreg_ptr;    
 
     int fd = -1;
@@ -52,17 +67,22 @@ int main() {
         return(-1);
     }
 
-    data_a_ptr = (int *) (LW_virtual + data_a_base);
-    data_b_ptr = (int *) (LW_virtual + data_b_base);
+    data_a_ptr = (int *) (LW_virtual + DATA_A_BASE);
+    data_b_ptr = (int *) (LW_virtual + DATA_B_BASE);
     wrreg_ptr = (int *) (LW_virtual + WERREG);
 
-    //amarelo/verde neon
-    set_background_color (7, 2, 3, data_a_ptr, data_b_ptr, wrreg_ptr); //
+
+    //cor DarkMagenta
+    set_background_color (139, 0, 139, data_a_ptr, data_b_ptr, wrreg_ptr);
     
     //sprite coração
     set_sprite (1, 320, 445, 15, 1, data_a_ptr, data_b_ptr, wrreg_ptr);
 
-    // 0x00001
+    //edita um bloco para laranja
+    set_background_block (5, 3, 255, 140, 0, data_a_ptr, data_b_ptr, wrreg_ptr);
+
+    
+
     if (munmap (LW_virtual, LW_BRIDGE_SPAN) != 0) {
         printf("ERROR: munmap() failed...\n");
         return(-1);
