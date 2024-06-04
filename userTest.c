ea992797d12a77c2f9c54d9d10a4e75c55069c9f
Leticia Ribeiro <68934064+leticiaribeiro7@users.noreply.github.com>
2024-06-04 11:55:43 -0300

    Update userTest.c

--- userTest.c
+++ userTest.c
@@ -2,19 +2,22 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
-int8_t write_buf[64]; // cada char tem 8 bits
+int8_t write_buf[64] = {"r: 1, g: 2, b: 3"}; // cada char tem 8 bits
 
 
 int main() {
     int fd;
-    fd = open("/dev/GP", O_RDWR);
-    if (fd < 0) {
-        return 0;
+    if ((fd = open("/dev/graphicProcessor", O_RDWR)) == -1){
+        printf("Error opening /dev/GP: %s\n", strerror(errno));
+        return -1;
     }
-    write_buf = "r: 1, g: 2, b: 3";
-    bytes_written = write(fd, write_buf, strlen(write_buf)+1);
+
+    write(fd, write_buf, strlen(write_buf)+1);
 
     close(fd);
+    return 0;
 
 }
