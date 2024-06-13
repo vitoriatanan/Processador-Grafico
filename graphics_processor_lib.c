783c01cf41772d173bf9ed7da42b42c42a6e5de9
Leticia Ribeiro <68934064+leticiaribeiro7@users.noreply.github.com>
2024-06-13 19:19:35 -0300

    Update graphics_processor_lib.c

--- graphics_processor_lib.c
+++ graphics_processor_lib.c
@@ -8,13 +8,14 @@
 #define WBR 1
 #define WBM 2
 #define DP 3
+#define WSM 4
 #define MAX_SIZE 32
 
 // prototipos
 int set_background_color(int R, int G, int B);
 int set_sprite(int reg, int x, int y, int offset, int activation_bit);
 int set_background_block(int column, int line, int R, int G, int B);
-int define_poligon(int forma, int R, int G, int B, int tamanho, int x, int y);
+int define_poligon(int forma, int R, int G, int B, int tamanho, int x, int y, int endereco);
 int write_sprite_mem (int R, int G, int B, int endereco_memoria);
 
 
@@ -77,7 +78,7 @@ int set_background_block(int column, int line, int R, int G, int B) {
     return bytesWritten;
 }
 
-int define_poligon(int forma, int R, int G, int B, int tamanho, int x, int y) {
+int define_poligon(int forma, int R, int G, int B, int tamanho, int x, int y, int endereco) {
     int fd;
     unsigned char buffer[MAX_SIZE];
 
@@ -86,7 +87,7 @@ int define_poligon(int forma, int R, int G, int B, int tamanho, int x, int y) {
         return -1;
     }
 
-    sprintf(buffer, "%d %d %d %d %d %d %d %d", DP, forma, R, G, B, tamanho, x, y);
+    sprintf(buffer, "%d %d %d %d %d %d %d %d %d", DP, forma, R, G, B, tamanho, x, y, endereco);
     int bytesWritten = write(fd, buffer, strlen(buffer));
 
     close(fd);
@@ -94,5 +95,17 @@ int define_poligon(int forma, int R, int G, int B, int tamanho, int x, int y) {
 }
 
 int write_sprite_mem(int R, int G, int B, int endereco_memoria) {
-    
+    int fd;
+    unsigned char buffer[MAX_SIZE];
+
+    if ((fd = open("/dev/graphicProcessor", O_RDWR)) == -1){
+        printf("Error opening /dev/graphicProcessor: %s\n", strerror(errno));
+        return -1;
+    }
+
+    sprintf(buffer, "%d %d %d %d %d", WSM, R, G, B, endereco_memoria);
+    int bytesWritten = write(fd, buffer, strlen(buffer));
+
+    close(fd);
+    return bytesWritten;
 }
