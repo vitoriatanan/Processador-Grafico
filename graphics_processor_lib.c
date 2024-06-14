c3696576449f9ce83088eb2957e6633df2cc47e7
vitoriatanan <128485508+vitoriatanan@users.noreply.github.com>
2024-06-14 17:31:56 -0300

    Update graphics_processor_lib.c

--- graphics_processor_lib.c
+++ graphics_processor_lib.c
@@ -14,7 +14,7 @@
 // prototipos
 int set_background_color(int R, int G, int B);
 int set_sprite(int reg, int x, int y, int offset, int activation_bit);
-int set_background_block(int column, int line, int R, int G, int B);
+int set_background_block(int endereco_memoria, int R, int G, int B);
 int define_poligon(int forma, int R, int G, int B, int tamanho, int x, int y, int endereco);
 int write_sprite_mem (int R, int G, int B, int endereco_memoria);
 
@@ -22,11 +22,54 @@ int write_sprite_mem (int R, int G, int B, int endereco_memoria);
 int main() {
 
     //int bytesWritten = set_background_color(100, 245, 345);
-    int bytesWritten = set_sprite (1, 320, 445, 9, 1);
+    //int bytesWritten = set_sprite (1, 320, 445, 9, 1);
     //int bytesWritten = set_background_block(5, 3, 255, 0, 0);
-    printf("%d bytes escritos\n", bytesWritten);
+    //printf("%d bytes escritos\n", bytesWritten);
 
-    return 0;
+    //   //Azul claro (ceu)    set_cor_background_wbr(6, 4, 3)
+    // cor da grama: 1,7,1
+
+
+
+    //verde escuro 1 6 1
+    set_background_color(1 ,4, 7);
+    
+    
+    //grama 
+    int i;
+    for (i = 4000; i < 4800; i++) {
+        set_background_block(i, 1, 6, 0);
+    }
+
+    //patinhas
+    define_poligon(0, 1, 6, 0, 1, 200, 402, 0); //quadrado 1
+    define_poligon(0, 1, 6, 0, 1, 260, 402, 1);
+    //set_background_block(3947, 1, 7, 1); //3951
+
+    //entre as patas
+    for (i = 3946; i < 3952; i++){
+       set_background_block(i, 1, 7, 1);
+    }
+
+    //rabinho
+    set_background_block(3863, 7, 1, 1); 
+
+    //pele acima da pata
+    for (i = 3864; i < 3874; i++) {
+        set_background_block(i, 6, 4, 3);
+    }
+    
+    //casco
+    //define_poligon(0, 1, 6, 0, 1, 200, 402, 0); 
+    
+
+   
+
+     //primeira linha: 0 - 78
+    //segunda linha: 80 + 78 = 158
+    //0 - 4799
+    //set_background_block(4001, 1, 7, 1);
+    
 
 }
 
@@ -62,7 +105,7 @@ int set_sprite(int reg, int x, int y, int offset, int activation_bit) {
 
 }
 
-int set_background_block(int column, int line, int R, int G, int B) {
+int set_background_block(int endereco_memoria, int R, int G, int B) {
     int fd;
     unsigned char buffer[MAX_SIZE];
 
@@ -71,7 +114,7 @@ int set_background_block(int column, int line, int R, int G, int B) {
         return -1;
     }
 
-    sprintf(buffer, "%d %d %d %d %d %d", WBM, column, line, R, G, B);
+    sprintf(buffer, "%d %d %d %d %d", WBM, endereco_memoria, R, G, B);
     int bytesWritten = write(fd, buffer, strlen(buffer));
 
     close(fd);
