65688aef0065cf06c2555f3e36a629d80a200c03
vitoriatanan <128485508+vitoriatanan@users.noreply.github.com>
2024-06-17 12:27:20 -0300

    Update graphics_processor_lib.c

--- graphics_processor_lib.c
+++ graphics_processor_lib.c
@@ -34,7 +34,8 @@ int main() {
     //verde escuro 1 6 1
     set_background_color(1 ,4, 7);
     
-    
+    //set_background_block(0, 3, 6, 0); teste de cor verde olivia
+
     //grama 
     int i;
     for (i = 4000; i < 4800; i++) {
@@ -42,27 +43,58 @@ int main() {
     }
 
     //patinhas
-    define_poligon(0, 1, 6, 0, 1, 200, 402, 0); //quadrado 1
-    define_poligon(0, 1, 6, 0, 1, 260, 402, 1);
+    define_poligon(0, 1, 6, 0, 1, 200, 402, 0); //patinha 1
+    define_poligon(0, 1, 6, 0, 1, 260, 402, 1); //patinha 2
     //set_background_block(3947, 1, 7, 1); //3951
 
     //entre as patas
     for (i = 3946; i < 3952; i++){
-       set_background_block(i, 1, 7, 1);
+       set_background_block(i, 0, 7, 0);
     }
 
     //rabinho
-    set_background_block(3863, 7, 1, 1); 
+    set_background_block(3863, 0, 7, 0); 
 
     //pele acima da pata
     for (i = 3864; i < 3874; i++) {
-        set_background_block(i, 6, 4, 3);
+        set_background_block(i, 7, 3, 3);
     }
     
+    //3794 cabeça (1 bloco mais a cima da pele)
+
+
+    //set_background_block(3793, 7, 2, 1);
+
+    //casco até 3793 -> primeira linha casco
+    for (i = 3784; i < 3794; i++) {
+        set_background_block(i, 5, 2, 2);
+    }
+
+    //->segunda linha casco
+    set_background_block(3700, 6, 4, 2);
+    
+    /*
+    set_background_block(0, 4, 2, 2); //lilás
+    set_background_block(2, 6, 4, 2);
+    set_background_block(6, 7, 2, 1);
+    set_background_block(8, 6, 3, 1);
+    set_background_block(10, 7, 2, 2); //laranja
+    set_background_block(12, 5, 3, 2); //verde limão
+    set_background_block(14, 5, 2, 2); //marrom
+    set_background_block(16, 6, 3, 2); 
+    set_background_block(18, 4, 2, 2); */
+    
+
+
+
+    //define_poligon(0, 7, 3, 3, 1, 150, 325, 4);
+    //set_background_block(0, 2, 3, 1);
+    //set_background_block(1, 0, 7, 0);
     //casco
     //define_poligon(0, 1, 6, 0, 1, 200, 402, 0); 
     
-
+    //g b r
+    //define_poligon(0, 7, 3, 3, 1, 260, 280, 5);
    
 
      //primeira linha: 0 - 78
