a23b0e8345843aae062cad15394b6dc6bb8c10ab
vitoriatanan <128485508+vitoriatanan@users.noreply.github.com>
2024-06-19 20:02:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdlib.h>
 #include "graphics_processor.h"
 
+/**
+ * Chamada das funções para setar elementos e realizar o desenho
+ * 
+ * Desenhos: grama, tartaruga, casa, sol e sprites de árvores.
+*/
 int main() {
-
-    /**
-     * Chamada das funções para setar elementos e realizar o desenho
-     * 
-     * Desenhos: grama, tartaruga,casa, sol e sprites de árvores.
-    */
-
+    
     // Céu
     set_background_color(1, 4, 7);
 
@@ -18,26 +17,26 @@ int main() {
         set_background_block(i, 1, 6, 0);
     }
 
-    /*    TARTARUGA     */
-    // patinhas
+    /*                   TARTARUGA                      */
+    // Patinhas
     define_poligon(0, 1, 6, 0, 1, 200, 402, 0); // patinha 1
     define_poligon(0, 1, 6, 0, 1, 260, 402, 1); // patinha 2
-    // set_background_block(3947, 1, 7, 1); //3951
 
-    // entre as patas
+    // Entre as patas
     for (i = 3946; i < 3952; i++) {
         set_background_block(i, 0, 7, 0);
     }
 
-    // rabinho
+    // Rabinho
     set_background_block(3863, 0, 7, 0);
 
-    // pele acima da pata
+    
+    // Pele acima da pata
     for (i = 3864; i < 3874; i++) {
         set_background_block(i, 7, 3, 3);
     }
 
-    // CASCO
+    // Casco
     for (i = 3784; i < 3794; i++) {
         set_background_block(i, 5, 2, 2);
     }
@@ -58,7 +57,7 @@ int main() {
         set_background_block(i, 5, 2, 2);
     }
 
-    // CABEÇA
+    // Cabeça
     for (i = 3874; i < 3877; i++) {
         set_background_block(i, 0, 7, 0);
     }
@@ -73,35 +72,31 @@ int main() {
         set_background_block(i, 0, 7, 0);
     }
 
-    // olho
+    // Olho
     set_background_block(3716, 0, 0, 0);
-  
-    // MENOR REGISTRADOR FICA POR CIMA
 
 
-    //define_poligon(int forma, int R, int G, int B, int tamanho, int x, int y, int endereco);
-
-
-    // CASA
+    /*                  CASA                 */
     //  Telhado
     define_poligon(1, 6, 3, 2, 15, 460, 220, 4);
 
-    // Casa
+    // Estrutura
     define_poligon(0, 7, 7, 7, 13, 460, 350, 3);
 
     //  Porta
     define_poligon(0, 0, 0, 0, 3, 460, 400, 5);
 
-    // SOL
+    
+    /*                SOL                 */
     define_poligon(0, 7, 7, 0, 5, 50, 50, 6);
 
-    //SPRITES: árvore
+    
+    /*      SPRITE:  ÁRVORE      */
     set_sprite(1, 300, 385 , 4, 1);
     set_sprite(2, 100, 385 , 4, 1);
     set_sprite(3, 50, 430 , 4, 1);
     set_sprite(4, 220, 450 , 4, 1);
     set_sprite(5, 560, 430 , 4, 1);
-
 
     return 0;
 }
